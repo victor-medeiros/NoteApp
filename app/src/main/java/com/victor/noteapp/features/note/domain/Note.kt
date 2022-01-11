e7c2ed14db2adabc3157a09c87f984105c7fa0d4
victor-medeiros <victor.msilva@bandtec.com.br>
2022-01-11 19:47:24 -0300

    Setting note entity and possible note colors

---
+++ app/src/main/java/com/victor/noteapp/features/note/domain/Note.kt
@@ -0,0 +1,20 @@
+package com.victor.noteapp.features.note.domain
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.victor.noteapp.ui.theme.LightBlue
+import com.victor.noteapp.ui.theme.LightGreen
+import com.victor.noteapp.ui.theme.Red
+import com.victor.noteapp.ui.theme.Yellow
+
+@Entity
+data class Note(
+    val title: String,
+    val content: String,
+    val color: Int,
+    @PrimaryKey val id: Int? = null
+) {
+    companion object {
+        val colors = listOf(LightGreen, LightBlue, Yellow, Red)
+    }
+}
--- app/src/main/java/com/victor/noteapp/ui/theme/Color.kt
+++ app/src/main/java/com/victor/noteapp/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.victor.noteapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val LightGreen = Color(0xFF36CF64)
+val Yellow = Color(0xFFCFAB36)
+val Red = Color(0xFFCF3650)
+val LightBlue = Color(0xFF36AECF)
