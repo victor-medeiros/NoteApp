2e7f44864b2707f5381c6a0d32a52a540f7a8010
victor-medeiros <victor.msilva@bandtec.com.br>
2022-01-19 23:05:42 -0300

    Creating transparent text field component

---
+++ app/src/main/java/com/victor/noteapp/features/note/presentation/add_edit_note/components/TransparentHintTextField.kt
@@ -0,0 +1,46 @@
+package com.victor.noteapp.features.note.presentation.add_edit_note.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusState
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+
+@Composable
+fun TransparentHintTextField(
+    text: String,
+    hint: String,
+    isHintVisible: Boolean,
+    modifier: Modifier = Modifier,
+    textStyle: TextStyle,
+    singleLine: Boolean,
+    maxLines: Int = 10,
+    onTextChange: (String) -> Unit,
+    onFocusChange: (FocusState) -> Unit
+) {
+    Box(modifier = modifier) {
+        BasicTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .onFocusChanged {
+                    onFocusChange
+                },
+            value = text,
+            textStyle = textStyle,
+            singleLine = singleLine,
+            onValueChange = onTextChange
+        )
+        if (isHintVisible) {
+            Text(
+                text = hint,
+                style = textStyle,
+                color = Color.DarkGray
+            )
+        }
+    }
+}
