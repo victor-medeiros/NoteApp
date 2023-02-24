bf9503d15dc0e16d95d7a2d3e753bcac93b1c16a
victor-medeiros <victor.medeirossivla.ext@bayer.com>
2023-02-24 00:39:26 -0300

    :lipstick: Fixing NoteItem component style

--- app/src/main/java/com/victor/noteapp/features/note/presentation/notes/components/NoteItem.kt
+++ app/src/main/java/com/victor/noteapp/features/note/presentation/notes/components/NoteItem.kt
@@ -1,97 +1,86 @@
 package com.victor.noteapp.features.note.presentation.notes.components
 
-import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.drawscope.clipPath
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.core.graphics.ColorUtils
 import com.victor.noteapp.features.note.domain.model.Note
+import com.victor.noteapp.ui.theme.LightBlue
 
 @Composable
 fun NoteItem(
+    modifier: Modifier = Modifier,
     note: Note,
     cornerRadius: Dp = 10.dp,
-    cutCornerSize: Dp = 30.dp,
     onDelete: () -> Unit,
-    modifier: Modifier = Modifier
 ) {
-    Box(
-        modifier = modifier
+    Surface(
+        modifier = modifier,
+        shape = RoundedCornerShape(cornerRadius),
+        elevation = 16.dp
     ) {
-        Canvas(
-            modifier = Modifier.matchParentSize()
-        ) {
-            val clipPath = Path().apply {
-                lineTo(0f, size.width - cutCornerSize.toPx())
-                lineTo(size.width, cutCornerSize.toPx())
-                lineTo(size.width, size.height)
-                lineTo(0f, size.height)
-                close()
+        Box {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(note.color))
+                    .padding(16.dp)
+                    .padding(end = 32.dp)
+            ) {
+                Text(
+                    text = note.title,
+                    style = MaterialTheme.typography.h6,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    color = MaterialTheme.colors.onSurface
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = note.content,
+                    style = MaterialTheme.typography.body1,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    color = MaterialTheme.colors.onSurface
+                )
             }
+            IconButton(
+                onClick = onDelete,
+                modifier = Modifier.align(Alignment.BottomEnd)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "Delete note"
+                )
+            }
+        }
+    }
+}
 
-            clipPath(clipPath) {
-                drawRoundRect(
-                    color = Color(note.color),
-                    cornerRadius = CornerRadius(cornerRadius.toPx()),
-                    size = size
-                )
-                drawRoundRect(
-                    color = Color(
-                        ColorUtils.blendARGB(note.color, 0x000000, 0.2f)
-                    ),
-                    topLeft = Offset(size.width - cutCornerSize.toPx(), -100f),
-                    size = Size(cutCornerSize.toPx() + 100f, cutCornerSize.toPx() + 100f),
-                    cornerRadius = CornerRadius(cornerRadius.toPx())
-                )
-            }
-            
-        }
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)
-                .padding(end = 32.dp)
-        ) {
-            Text(
-                text = note.title,
-                style = MaterialTheme.typography.h6,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                color = MaterialTheme.colors.onSurface
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = note.content,
-                style = MaterialTheme.typography.body1,
-                maxLines = 10,
-                overflow = TextOverflow.Ellipsis,
-                color = MaterialTheme.colors.onSurface
-            )
-        }
-        IconButton(
-            onClick = onDelete,
-            modifier = Modifier.align(Alignment.BottomEnd)
-        ) {
-            Icon(
-                imageVector = Icons.Default.Delete,
-                contentDescription = "Delete note"
-            )
-        }
+@Preview(widthDp = 400, heightDp = 100, showBackground = true)
+@Composable
+fun NoteItemPreview() {
+    Surface() {
+        NoteItem(
+            note = Note(
+                title = "Teste",
+                content = LoremIpsum(words = 50).values.first(),
+                timestamp = System.currentTimeMillis(),
+                color = LightBlue.toArgb()
+            ),
+            modifier = Modifier.padding(8.dp),
+            onDelete = {  }
+        )
     }
 }
